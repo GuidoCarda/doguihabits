1ff6b75b67c84f9f30dc2b3d2bd7324bc1dcfc3f
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-08 13:37:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="min-h-screen bg-slate-800"></div>;
+}
+
+export default App;
---
+++ src/store.jsx
@@ -0,0 +1,5 @@
+import { create } from "zustand";
+
+const useHabitsStore = create((set) => ({}));
+
+export default useHabitsStore;
