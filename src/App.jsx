9e71d85652683da584002d114665804111e9dc2f
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-08 20:17:00 -0300

    habits store implementation sort of?

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
 import "./App.css";
+import useHabitsStore from "./store";
+import { shallow } from "zustand/shallow";
 
 function App() {
-  return <div className="min-h-screen bg-slate-800"></div>;
+  const habits = useHabitsStore((state) => state.habits);
+  const createHabit = useHabitsStore((state) => state.createHabit);
+
+  console.log(habits);
+
+  return (
+    <main className="min-h-screen bg-neutral-800 text-neutral-50">
+      <section className="max-w-lg mx-auto py-10 px-4">
+        <h1 className="font-bold text-4xl mb-8">My habits</h1>
+        <div className="flex gap-4">
+          <Input />
+          <button
+            className="flex-shrink-0 px-6 font-bold h-12 bg-green-600 text-white   rounded-md"
+            onClick={createHabit}
+          >
+            add
+          </button>
+        </div>
+
+        <ul className="mt-10">
+          {Boolean(habits.length) &&
+            habits.map((habit, index) => (
+              <li key={index} className="flex flex-wrap gap-2">
+                {habit.days.map((day) => (
+                  <span className="h-10 w-10 flex flex-col bg-slate-700 rounded-md"></span>
+                ))}
+              </li>
+            ))}
+        </ul>
+      </section>
+    </main>
+  );
 }
+
+const Input = () => {
+  const [input, setInput] = useHabitsStore(
+    (state) => [state.input, state.setInput],
+    shallow
+  );
+
+  const handleChange = (e) => setInput(e.target.value);
+
+  return (
+    <>
+      <input
+        className="bg-neutral-600 w-full h-12 px-2 rounded-md outline-none border-none "
+        type="text"
+        value={input}
+        onChange={handleChange}
+      />
+    </>
+  );
+};
 
 export default App;
--- src/store.jsx
+++ src/store.jsx
@@ -1,5 +1,37 @@
 import { create } from "zustand";
 
-const useHabitsStore = create((set) => ({}));
+const day = {
+  id: 1,
+  state: "pending",
+};
+
+const habit = {
+  id: "",
+  title: "",
+  days: Array(30)
+    .fill("")
+    .map((_, idx) => ({ id: idx, state: "pending" })),
+};
+
+const useHabitsStore = create((set, get) => ({
+  habits: [],
+  input: "",
+  setInput: (userInput) => set(() => ({ input: userInput })),
+  createHabit: () =>
+    set(() => ({
+      habits: [
+        ...get().habits,
+        { ...habit, id: randomId(), title: get().input },
+      ],
+      input: "",
+    })),
+  updateHabit: () => set(() => ({})),
+}));
 
 export default useHabitsStore;
+
+const randomId = (length = 6) => {
+  return Math.random()
+    .toString(36)
+    .substring(2, length + 2);
+};
