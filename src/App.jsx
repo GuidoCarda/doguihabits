9d73fec2da82b541f2718d42c31994da4550e925
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-13 17:31:09 -0300

    Tailwind-Scrollbars

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ function App() {
   return (
     <main className="min-h-screen bg-zinc-800">
       <Layout>
-        {showHabitForm && <Modal onClose={handleShowToggle} />}
         <div className="flex items-center justify-between mb-10">
           <h1 className="text-3xl font-semibold text-neutral-100">My Habits</h1>
 
@@ -44,7 +43,8 @@ function App() {
           </button>
         </div>
 
-        {showHabitForm && <HabitForm handleClose={handleShowToggle} />}
+        {showHabitForm && <Modal onClose={handleShowToggle} />}
+        {/* {showHabitForm && <HabitForm handleClose={handleShowToggle} />} */}
 
         {habits.length > 1 && <HabitsSorting handleSort={handleSort} />}
 
@@ -76,7 +76,7 @@ const Habit = (props) => {
   const currentDate = new Date().getDate();
 
   return (
-    <div className="bg-zinc-600 px-6   py-6 rounded-md">
+    <div className="bg-zinc-600 px-6 py-6 rounded-md ">
       <header className="flex justify-between items-center mb-6">
         <h2 className="text-2xl  font-bold capitalize">{habit.title}</h2>
         <button
@@ -110,7 +110,7 @@ const Habit = (props) => {
   );
 };
 
-const HabitForm = ({ handleClose }) => {
+const HabitForm = ({ onClose }) => {
   const [input, setInput] = useHabitsStore(
     (state) => [state.input, state.setInput],
     shallow
@@ -121,11 +121,11 @@ const HabitForm = ({ handleClose }) => {
   const onClick = () => {
     if (!input.trim()) return alert("empty field");
     createHabit();
-    handleClose();
+    onClose();
   };
 
   return (
-    <div className="flex gap-2 my-6">
+    <div className="flex flex-col">
       <div className="w-full">
         <label htmlFor="habit" className="text-neutral-400 mb-1 block">
           habit title
@@ -139,7 +139,7 @@ const HabitForm = ({ handleClose }) => {
         />
       </div>
       <button
-        className="h-12 w-min px-6 self-end bg-green-600 text-white font-bold rounded-md"
+        className="h-12 w-min px-6 mt-6 ml-auto bg-green-600 text-white font-bold rounded-md"
         onClick={onClick}
       >
         add
@@ -179,56 +179,19 @@ const Modal = ({ onClose }) => {
   return ReactDOM.createPortal(
     <>
       <div className="fixed grid place-items-center top-0 left-0 right-0 bottom-0 h-screen w-full bg-black/50 backdrop-blur-[2px] px-4">
-        <div className=" bg-zinc-600 rounded-md p-4 w-full sm:max-w-md overflow-y-scroll h-96">
-          <h2 className="text-2xl text-neutral-300 font-semibold">
-            Crea tu habito:
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium quasi nostrum fuga modi ducimus architecto, iste,
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <button onClick={onClose}>cerrar </button>
-          </h2>
+        <div className=" bg-zinc-600 rounded-md px-6 py-10 w-full sm:max-w-md overflow-y-scroll scrollbar-thin scrollbar-thumb-zinc-500 scrollbar-thumb-rounded-md">
+          <div className="flex items-center justify-between mb-8">
+            <h2 className="text-2xl text-neutral-300 font-semibold">
+              New Habit
+            </h2>
+            <button
+              className="h-8 w-8 rounded-md bg-zinc-400 font-bold text-black/50"
+              onClick={onClose}
+            >
+              X
+            </button>
+          </div>
+          <HabitForm onClose={onClose} />
         </div>
       </div>
     </>,
