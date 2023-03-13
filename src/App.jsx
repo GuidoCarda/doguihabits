93384a25a639d8c91b2369eb641fa74c2971afd8
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-13 19:00:04 -0300

    useClickOutside hook to manage closing modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import useHabitsStore, { useHabitsActions } from "./store/store";
 
-import ReactDOM from "react-dom";
-
 import { shallow } from "zustand/shallow";
 import { useMemo, useState } from "react";
 import Layout from "./components/Layout";
+import Modal from "./components/Modal";
+import useClickOutside from "./hooks/useClickOutside";
 
 function App() {
   const [showHabitForm, setShowHabitForm] = useState(false);
@@ -43,8 +43,7 @@ function App() {
           </button>
         </div>
 
-        {showHabitForm && <Modal onClose={handleShowToggle} />}
-        {/* {showHabitForm && <HabitForm handleClose={handleShowToggle} />} */}
+        <Modal open={showHabitForm} onClose={() => setShowHabitForm(false)} />
 
         {habits.length > 1 && <HabitsSorting handleSort={handleSort} />}
 
@@ -110,7 +109,7 @@ const Habit = (props) => {
   );
 };
 
-const HabitForm = ({ onClose }) => {
+export const HabitForm = ({ onClose }) => {
   const [input, setInput] = useHabitsStore(
     (state) => [state.input, state.setInput],
     shallow
@@ -172,30 +171,6 @@ const HabitsSorting = ({ handleSort }) => {
         ))}
       </div>
     </div>
-  );
-};
-
-const Modal = ({ onClose }) => {
-  return ReactDOM.createPortal(
-    <>
-      <div className="fixed grid place-items-center top-0 left-0 right-0 bottom-0 h-screen w-full bg-black/50 backdrop-blur-[2px] px-4">
-        <div className=" bg-zinc-600 rounded-md px-6 py-10 w-full sm:max-w-md overflow-y-scroll scrollbar-thin scrollbar-thumb-zinc-500 scrollbar-thumb-rounded-md">
-          <div className="flex items-center justify-between mb-8">
-            <h2 className="text-2xl text-neutral-300 font-semibold">
-              New Habit
-            </h2>
-            <button
-              className="h-8 w-8 rounded-md bg-zinc-400 font-bold text-black/50"
-              onClick={onClose}
-            >
-              X
-            </button>
-          </div>
-          <HabitForm onClose={onClose} />
-        </div>
-      </div>
-    </>,
-    document.getElementById("portal")
   );
 };
 
---
+++ src/components/Modal.jsx
@@ -0,0 +1,37 @@
+import React, { forwardRef } from "react";
+import ReactDOM from "react-dom";
+import { HabitForm } from "../App";
+import useClickOutside from "../hooks/useClickOutside";
+
+const Modal = ({ open, onClose }) => {
+  const domNode = useClickOutside(onClose);
+
+  if (!open) return;
+
+  return ReactDOM.createPortal(
+    <>
+      <div className="fixed grid place-items-center top-0 left-0 right-0 bottom-0 h-screen w-full bg-black/50 backdrop-blur-[2px] px-4">
+        <div
+          ref={domNode}
+          className=" bg-zinc-600 rounded-md px-6 py-10 w-full sm:max-w-md overflow-y-scroll scrollbar-thin scrollbar-thumb-zinc-500 scrollbar-thumb-rounded-md"
+        >
+          <div className="flex items-center justify-between mb-8">
+            <h2 className="text-2xl text-neutral-300 font-semibold">
+              New Habit
+            </h2>
+            <button
+              className="h-8 w-8 rounded-md bg-zinc-400 font-bold text-black/50"
+              onClick={onClose}
+            >
+              X
+            </button>
+          </div>
+          <HabitForm onClose={onClose} />
+        </div>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+};
+
+export default Modal;
