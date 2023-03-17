16f07c1ed12143749aae0f550f412259088cfa09
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-17 20:20:18 -0300

    Quick layout fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import Habits from "./pages/habits";
 import Playground from "./pages/Playground";
 import HabitDetail from "./pages/HabitDetail";
-import Layout from "./components/Layout";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Habits />} />
-        <Route path="/habits/:id" element={<HabitDetail />} />
-        <Route path="/playground" element={<Playground />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Habits />} />
+      <Route path="/habits/:id" element={<HabitDetail />} />
+      <Route path="/playground" element={<Playground />} />
+    </Routes>
   );
 }
 
--- src/pages/HabitDetail.jsx
+++ src/pages/HabitDetail.jsx
@@ -5,9 +5,7 @@ import useHabitsStore from "../store/store";
 const HabitDetail = () => {
   let { id } = useParams();
 
-  console.log(id);
   const habits = useHabitsStore((state) => state.habits);
-
   const habit = habits.find((habit) => habit.id === id);
 
   return <div>{habit && <h1>{habit.title}</h1>}</div>;
