095188f75d1b89ad9ce61c2ab9ba247f77f47606
Guido Cardarelli <guidocarda@hotmail.com>
2023-04-10 13:38:11 -0300

    Habit form input length feedback
    - Disable add btn when the input length exceeds a given number.
    - Show chars left on input

--- src/pages/components/HabitForm.jsx
+++ src/pages/components/HabitForm.jsx
@@ -2,6 +2,7 @@ import { shallow } from "zustand/shallow";
 import useHabitsStore, { useHabitsActions } from "../../store/useHabitsStore";
 import toast from "react-hot-toast";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import clsx from "clsx";
 
 const HabitForm = ({ onClose }) => {
   const [input, setInput] = useHabitsStore(
@@ -30,9 +31,23 @@ const HabitForm = ({ onClose }) => {
   return (
     <div className="flex flex-col">
       <div className="w-full">
-        <label htmlFor="habit" className="text-neutral-400 mb-1 block">
-          habit title
-        </label>
+        <div className="flex justify-between">
+          <label htmlFor="habit" className="text-neutral-400 mb-1 block">
+            habit title
+          </label>
+
+          <span
+            className={clsx(
+              `text-sm text-zinc-400 font-semibold mt-1 mr-1 block w-4 text-right`,
+              {
+                "text-red-400": input.length > 30,
+              }
+            )}
+          >
+            {30 - input.length}
+          </span>
+        </div>
+
         <input
           className="h-12 rounded-md  bg-zinc-600 text-neutral-200 px-4 outline-none w-full"
           type="text"
@@ -43,8 +58,12 @@ const HabitForm = ({ onClose }) => {
         />
       </div>
       <button
-        className="h-12 w-min px-6 mt-6 ml-auto bg-green-600 text-white font-bold rounded-md"
+        className={clsx(
+          "h-12 w-min px-6 mt-6 ml-auto bg-green-600 text-white font-bold rounded-md",
+          "disabled:bg-zinc-600 disabled:text-zinc-400 disabled:cursor-not-allowed"
+        )}
         onClick={onClick}
+        disabled={input.length > 30}
       >
         add
       </button>
