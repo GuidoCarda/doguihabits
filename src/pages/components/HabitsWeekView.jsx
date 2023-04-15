1b7564797faa3cfffc62a041a2192e97482eeffa
Guido Cardarelli <guidocarda@hotmail.com>
2023-04-14 21:39:49 -0300

    HabitWeeklyView UI improved
    Due to having a lot of comments saying that they did not understand what this component was I decided to re-think the component structure and add details to give the user a better context.
    
    - Added createdAt badge
    - Added weekday badge to each day
    - Added current running streak and completion percentage

--- src/pages/components/HabitsWeekView.jsx
+++ src/pages/components/HabitsWeekView.jsx
@@ -11,8 +11,15 @@ import clsx from "clsx";
 import { Link } from "react-router-dom";
 
 //Icons
-import { TrashIcon } from "@heroicons/react/24/outline";
+import {
+  CheckBadgeIcon,
+  CheckIcon,
+  FireIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 import { toast } from "react-hot-toast";
+
+const weekDays = ["Sab", "Dom", "Lun", "Mar", "Mie", "Jue", "Vie"];
 
 const HabitsWeekView = ({ habit }) => {
   const { updateHabit, deleteHabit } = useHabitsActions();
@@ -59,16 +66,19 @@ const HabitsWeekView = ({ habit }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       className={clsx(
-        "bg-zinc-600 rounded-xl p-4 text-neutral-100 space-y-4 w-full  mx-auto",
+        "bg-zinc-600 rounded-xl text-neutral-100 space-y-4 w-full  mx-auto",
         "md:max-w-max md:mx-0"
       )}
     >
-      <div className="flex items-center justify-between">
+      <div className="flex px-4 pt-4 items-center justify-between">
         <Link
           to={`/habits/${habit.id}`}
           className="font-bold text-lg w-full block truncate"
         >
           {habit.title}
+          <span className="text-xs bg-zinc-400/40 text-zinc-300 font-normal w-max h-5 grid place-items-center px-1 rounded-sm">
+            created at {getDayMonthYear(habit.createdAt).join("/")}
+          </span>
         </Link>
         <button
           aria-label="delete habit"
@@ -79,25 +89,41 @@ const HabitsWeekView = ({ habit }) => {
         </button>
       </div>
 
-      <div className="grid grid-cols-7 gap-4">
-        {lastWeek.map(({ id, state }) => {
+      <div className="grid grid-cols-7 gap-4 px-4">
+        {lastWeek.map(({ id, state }, idx) => {
           const [day] = getDayMonthYear(id);
 
           return (
-            <button
-              aria-label="toggle habit state"
-              onClick={() => updateHabit(habit.id, id)}
-              key={`day-${id}`}
-              className={clsx("rounded-md h-10 w-10 font-semibold", {
-                "bg-success": state === "completed",
-                "bg-failed": state === "failed",
-                "bg-zinc-700": state !== "failed" && state !== "completed",
-              })}
-            >
-              {day}
-            </button>
+            <div>
+              <span className="block text-xs w-full text-center font-semibold text-zinc-400 pb-1">
+                {weekDays[idx]}
+              </span>
+              <button
+                aria-label="toggle habit state"
+                onClick={() => updateHabit(habit.id, id)}
+                key={`day-${id}`}
+                className={clsx("rounded-md h-10 w-10 font-semibold", {
+                  "bg-success": state === "completed",
+                  "bg-failed": state === "failed",
+                  "bg-zinc-700": state !== "failed" && state !== "completed",
+                })}
+              >
+                {day}
+              </button>
+            </div>
           );
         })}
+      </div>
+
+      <div className="border-t-2 px-4 pb-2 pt-2 border-zinc-500/50 flex gap-2">
+        <div className="flex items-center gap-1">
+          <FireIcon className="h-5 w-5 text-red-500" strokeWidth="2" />
+          <span className="text-smtext-zinc-300 font-semibold">3</span>
+        </div>
+        <div className="flex items-center gap-1 ">
+          <CheckBadgeIcon className="h-5 w-5 text-green-500" strokeWidth="2" />
+          <span className="text-smtext-zinc-300 font-semibold">46%</span>
+        </div>
       </div>
     </motion.div>
   );
