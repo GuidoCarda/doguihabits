cba1dc179601fd9b0483d0bef57d4d4ab2bf9ee2
Guido Cardarelli <guidocarda@hotmail.com>
2023-06-24 21:34:15 -0300

    .

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,6 @@ import useDialogStore from "../store/useDialogStore";
 
 //Components
 import ConfirmationDialog from "./ConfirmationDialog";
-import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
   const { open, state, handleClose, handleSubmit } = useDialogStore();
@@ -17,20 +16,9 @@ const Layout = ({ children }) => {
     cancelBtnRef.current.focus();
   }, [open]);
 
-  // const toastOptions = {
-  //   style: {
-  //     backgroundColor: "hsl(240, 3.8297872340425574%, 30%)",
-  //     color: "hsl(240, 3.8297872340425574%, 85%)",
-  //   },
-  //   error: {
-  //     duration: 1500,
-  //   },
-  // };
-
   return (
     <section className="max-w-screen-xl px-3 mx-auto py-12">
       {children}
-      {/* <Toaster toastOptions={toastOptions} /> */}
       <ConfirmationDialog
         ref={cancelBtnRef}
         open={open}
