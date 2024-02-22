55efe567b516adfe6ddc0da57a8ab078606e5119
Guido Cardarelli <guidocarda@hotmail.com>
2024-02-22 10:47:46 -0300

    feat(landing): basic boilerplate and structure

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,44 @@
+const Landing = () => {
+  return (
+    <main>
+      <Header />
+      <Layout>
+        <Hero />
+        <Features />
+        <HowItWorks />
+        <GetStarted />
+      </Layout>
+      <Footer />
+    </main>
+  );
+};
+
+const Header = () => {
+  return <header></header>;
+};
+
+const Hero = () => {
+  return <section></section>;
+};
+
+const Features = () => {
+  return <section></section>;
+};
+
+const Layout = () => {
+  return <div></div>;
+};
+
+const HowItWorks = () => {
+  return <section></section>;
+};
+
+const GetStarted = () => {
+  return <section></section>;
+};
+
+const Footer = () => {
+  return <Footer></Footer>;
+};
+
+export default Landing;
